9ec542424228fa0f57a7938b3934f7a02b756374
gurmanshaan <gurmanshaan@gmail.com>
2021-04-17 01:37:32 +0530

    create login component

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AuthProvider } from '../contexts/AuthContext';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
+function App() {
+  return (
+
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch >
+              <Route exact path="/home" component={Dashboard} />
+              <Route path="/" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
