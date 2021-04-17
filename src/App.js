4f9ea0178a76856df4cce4a667a52d903069ed60
gurmanshaan <gurmanshaan@gmail.com>
2021-04-18 01:25:57 +0530

    add home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { AuthProvider } from '../contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Login from './Login';
+import Home from './components/Home/Home';
+import Login from './components/Login';
+
 function App() {
   return (
 
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+    <Container className=" d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className="w-100" style={{ maxWidth: '400px' }}>
         <Router>
           <AuthProvider>
             <Switch >
-              <Route exact path="/home" component={Dashboard} />
+              <Route exact path="/home" component={Home} />
               <Route path="/" component={Login} />
             </Switch>
           </AuthProvider>
         </Router>
       </div>
-    </Container>
+    </Container >
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Title from './Title/Title'
+import './Home.css';
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <Title />
+        </div>
+    )
+}
