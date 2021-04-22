97984746407b28fccdfed25b931d6e2e31acf384
gurmanshaan <gurmanshaan@gmail.com>
2021-04-23 01:46:17 +0530

    Created Modal

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 import Title from './components/Title'
 import UploadForm from './components/UploadForm';
 import './Home.css';
+
 export default function Home() {
+    const [selectedImg, setSelectedImg] = useState(null);
+
     return (
         <div >
             <Title />
             <UploadForm />
-            <ImageGrid />
+            <ImageGrid setSelectedImg={setSelectedImg} />
+            {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
         </div>
     )
 }
--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import useFireStore from '../hooks/useFireStore';
 
-const ImageGrid = () => {
+const ImageGrid = ({ setSelectedImg }) => {
     const { docs } = useFireStore('images');
     return (
         <div>
             <div className="img-grid">
                 {docs && docs.map(doc => (
-                    <div className="img-wrap" key="doc.id">
+                    <div className="img-wrap" key="doc.id"
+                        onClick={() => setSelectedImg(doc.url)}
+                    >
                         <img src={doc.url} alt="uploaded pic" />
                     </div>
                 ))}
